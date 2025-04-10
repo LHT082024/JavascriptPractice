40b04e3c115c5a0729c9ed22732ca5892777b05a
LHT082024 <LHT082024@gmail.com>
2025-04-10 11:53:44 +0200

    trying to print line

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let fullname = "C11A7321T";
-let designation = "CAT";
+const fullname = "C11A7321T";
+const designation = "CAT";
+let age = 5;
+let previousOccupations = ["Testing subject for robot spec ops divison", "reserve military personal for spec ops", "Political assassin"];
+let currentOccupation = "$Crew onbard" + campaginShip;
+const campaginShip = "PlaceholderName"
+
+console.log(currentOccupation);
