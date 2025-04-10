29371028134154534fa57159494d8f407eaa3cd5
LHT082024 <LHT082024@gmail.com>
2025-04-10 13:54:37 +0200

    filling out the character sheet

--- DnDinSpace/script.js
+++ DnDinSpace/script.js
@@ -1,11 +1,16 @@
 //fetching html document
+const names = document.querySelector("#Name")
+const age = document.querySelector("#Occupation")
 const ship = document.querySelector("#Ship")
+
 
 
 //fetching Json
 fetch("./Json/cat.json")
   .then((res) => res.json())
   .then((data) => {
+    names.innerText = "name: " + data.fullname + "||" + " designation: " + data.designation;
+    age.innerText = "age: " + data.age;
     ship.innerText = "ship: " +  data.currentOccupation;
   })
   .catch((err) => console.error("Failed to load JSON:", err));
